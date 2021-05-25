4a814b22261f99c32d942144822aa8650e76676f
Simone Bogni <simone.bogni@gmail.com>
2021-05-25 21:49:52 +0300

    Fixed responsive behaviour in footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,12 @@
 <footer class="footer mt-auto py-2 fixed-bottom">
     <div class="container">
-        <span class="h5 d-inline-block">Bogni Simone - Fullstack Developer</span>
-        <span class="d-inline-block float-right text-right">Emek HaEla 5 - 714784 Modi'in - Israel
+        <!--small and less-->
+        <span class="h5 d-block text-center d-md-none">Bogni Simone - Fullstack Developer</span>
+        <span class="d-block text-center d-md-none">Emek HaEla 5 - 714784 Modi'in - Israel
+        </span>
+        <!--medium and more-->
+        <span class="h5 d-none d-md-inline-block">Bogni Simone - Fullstack Developer</span>
+        <span class="d-none d-md-inline-block float-right text-right">Emek HaEla 5 - 714784 Modi'in - Israel
         </span>
     </div>
 </footer>
