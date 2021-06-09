0a8eeb5992a7172b3fb08103c39c7ea483126a5a
Simone Bogni <simone.bogni@gmail.com>
2021-06-09 15:33:38 +0300

    Changed description in about.blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,13 +8,13 @@
         <div class="col-xs-12 col-md-8 offset-md-1 align-self-center">
             <div id="description">
                 <p><span role="img" aria-label="Waving hand">👋🏼</span> Hello, my name is Simone Bogni, I am a <strong>full stack webdeveloper</strong> with a good competence in various stages of development, including the design of the system and database.</p>
-                <p><span role="img" aria-label="Strong">💪🏼</span> I'm proficient in various programming languages and products and I have good <strong>problem solving skills</strong>.<br>
+                <p><span role="img" aria-label="Strong">💪🏼</span> I'm proficient in various programming languages, methodologies and products and I have good <strong>problem solving skills</strong>.<br>
                 I have a good proficiency in both <strong>back-end and front-end development</strong>, including with MVC-based frameworks.</p>
                 <p>I am <strong>responsible</strong> and <strong>independent</strong> and I can  effectively self-manage during projects, as well as collaborate in a team environment.</p>
                 <p><span role="img" aria-label="Books">📚</span> I'm <strong>open-minded</strong> and <strong>flexible</strong>, committed to the process of life-long learning, with an autodidact approach. I'm <strong>looking forward for more opportunities to learn</strong> and I have completed several courses on platforms such as EdX, Coursera, Udacity, Udemy and FreeCodeCamp.</p>
                 <p><span role="img" aria-label="University hat">🎓</span> I have attained the <strong>Bachelor diploma in Computer Science</strong> with a score of 95% and I have a business and economics background too.</p>
                 <p><span role="img" aria-label="Developer">👨🏻‍💻</span> So far I have built various projects including <strong>fully responsive websites</strong>, <strong>Progressive Web Applications</strong> and <strong>Java desktop apps</strong>.<br/>
-                I really love the <strong>feeling of accomplishment</strong> that I get when I bring a product from an idea to reality.</p>
+                I really love the <strong>feeling of accomplishment</strong> that comes from bringing a product from an idea to reality.</p>
             </div>
         </div>
     </div>
