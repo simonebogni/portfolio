229372468fe19c10c572bcce801b126c34d50689
Simone Bogni <simone.bogni@gmail.com>
2021-06-27 02:58:16 +0300

    Added padding classes to card content in Company component

--- resources/views/components/company.blade.php
+++ resources/views/components/company.blade.php
@@ -1,11 +1,11 @@
 <div class="card card-company">
-    <div class="card-header card-company-header">
-        <span class="company-name">{{$company->name}}</span><span class="company-location">, {{$company->city}} ({{$company->country}})</span>
+    <div class="card-header card-header-company">
+        <span class="company-name h4">{{$company->name}}</span><span class="company-location h5">, {{$company->city}} ({{$company->country}})</span>
     </div>
-    <div class="card-body card-company-body">
+    <div class="card-body card-body-company @if (isset($company->website)) pt-0 @endif">
         @if (isset($company->website))
         <div class="row">
-            <div class="col company-website">{{$company->website}}</div>
+            <div class="col company-website"><span class="workPlaceLink">{{$company->website}}</span></div>
         </div>
         @endif
         @if (isset($company->description))
