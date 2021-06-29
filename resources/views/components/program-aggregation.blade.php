5e5dea778227a01b86eb194bbf0204e2508dc923
Simone Bogni <simone.bogni@gmail.com>
2021-06-29 07:16:38 +0300

    Give correct classes to program-aggregation

--- resources/views/components/program-aggregation.blade.php
+++ resources/views/components/program-aggregation.blade.php
@@ -1,8 +1,8 @@
-<div class="card card-institute mb-3 w-100">
-    <div class="card-header card-header-institute">
-        <span class="institute-name h4">Other courses</span>
+<div class="card card-program-aggregation mb-3 w-100">
+    <div class="card-header card-header-program-aggregation">
+        <span class="program-aggregation-name h4">Other courses</span>
     </div>
-    <div class="card-body card-body-institute pt-0">
+    <div class="card-body card-body-program-aggregation pt-0">
         @if (isset($programs))
             @foreach ($programs->sortByDesc(function($p){
                 return (($p->current != null && $p->current) ? "1-" : "0-").($p->end_date != null ? $p->end_date->format('Y-m-d') : "0");
