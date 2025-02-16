272863d87e9de5b5a99f68538afbccf9509afdb7
simonebogni <simone.bogni.dev@gmail.com>
2025-02-16 03:07:12 +0200

    Remove hirak/prestissimo from Render deployment script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -10,4 +12,5 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate:fresh --force && php artisan db:seed --force
+php artisan migrate:fresh --force
+php artisan db:seed --force
