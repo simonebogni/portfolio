c4ab92c149baa34cc22e4e614a413dee51e0b8a7
simonebogni <simone.bogni.dev@gmail.com>
2025-02-23 01:12:48 +0200

    Add admin user settings and scoped factory. Create an admin user in the database seeder.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -41,4 +41,16 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function admin($name = "Admin", $email = "admin@example.com", $plainTextPassword = null): static
+    {
+        $userDetails = [
+          'name' => $name,
+          'email' => $email,
+        ];
+        if($plainTextPassword) {
+            $userDetails['password'] = Hash::make($plainTextPassword);
+        }
+        return $this->state(fn (array $attributes) => $userDetails);
+    }
 }
