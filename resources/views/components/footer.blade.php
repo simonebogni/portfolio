3cd638a35c1af565b757d40108c3d4ee997f763c
Simone Bogni <simonebogni@users.noreply.github.com>
2026-07-18 03:01:09 +0300

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="footer mt-auto py-2 fixed-bottom">
     <div class="container">
         <!--small and less-->
-        <span class="h5 d-block text-center d-md-none">Bogni Simone - Fullstack Developer</span>
-        <span class="d-block text-center d-md-none">Emek HaEla 5 - 714784 Modi'in - Israel
+        <span class="h5 d-block text-center d-md-none">Bogni Simone - Tech Lead, Fullstack Developer, Shopify Expert</span>
+        <span class="d-block text-center d-md-none">Tel Aviv - Israel
         </span>
         <!--medium and more-->
-        <span class="h5 d-none d-md-inline-block">Bogni Simone - Fullstack Developer</span>
-        <span class="d-none d-md-inline-block float-right text-right">Emek HaEla 5 - 7174784 Modi'in - Israel
+        <span class="h5 d-none d-md-inline-block">Bogni Simone - Tech Lead, Fullstack Developer, Shopify Expert</span>
+        <span class="d-none d-md-inline-block float-right text-right">Tel Aviv - Israel
         </span>
     </div>
 </footer>
